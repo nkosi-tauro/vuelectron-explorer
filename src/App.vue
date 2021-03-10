7c836aa3d12f7c59d7394b3a3c516a286e9f4c48
nkosi-tauro <nkosilati23@yahoo.com>
2021-03-10 21:30:00 +0200

    added methods to navigate, sort & get folder stats

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,55 @@
 <script>
 import fs from 'fs';
 import pathModule from 'path';
+import {computed, ref} from 'vue';
+import {app} from '@electron/remote'
+
+// convert size into readable units
+const formatSize = size => {
+  let i = Math.floor(Math.log(size)/ Math.log(1024))
+  return (
+    (size/ Math.pow(1024,i)).toFixed(2)* 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i]
+  )
+} 
 
 export default {
+  setup(){
+    const path = ref(app.getAppPath())
+    const files = computed(() => {
+      // get file path
+      const fileNames = fs.readdirSync(path.value)
+      return fileNames
+      // get file stats
+        .map(file => {
+          const stats = fs.statSync(pathModule.join(path.value, file))
+          return {
+            name : file,
+            size : stats.isFile() ? formatSize(stats.size ?? 0 ): null,
+            directory : stats.isDirectory()
+          }
+        })
+        // sort files (Folder first)
+        .sort((a,b) => {
+          if (a.directory === b.directory) {
+            return a.name.localeCompare(b.name)
+          }
+          return a.directory ? -1 : 1
+        })
+    })
 
+    // naviagate back
+    const back = () => {
+      path.value = pathModule.dirname(path.value)
+    }
+    // open folder
+    const open = folder => {
+      path.value = pathModule.join(path.value, folder)
+    }
+
+  
+
+    return {}
+  }
 }
 </script>
 
